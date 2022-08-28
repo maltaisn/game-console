df770512a91025eaf5b1bcae522bab08620cde91
maltaisn <nicolas.g.maltais@gmail.com>
2022-08-28 08:12:02 -0400

    TWorld: fix chip sound not played after last chip picked up

--- sw/app/tworld/src/tworld.c
+++ sw/app/tworld/src/tworld.c
@@ -1218,9 +1218,9 @@ static end_cause_t end_movement(moving_actor_t* act) {
         } else if (tile == TILE_THIEF) {
             tworld.boots = 0;
         } else if (tile == TILE_CHIP) {
+            tworld.events |= tworld.chips_left == 1 ? EVENT_LAST_CHIP_TAKEN : EVENT_CHIP_TAKEN;
             if (tworld.chips_left > 0) {
                 --tworld.chips_left;
-                tworld.events |= tworld.chips_left == 0 ? EVENT_LAST_CHIP_TAKEN : EVENT_CHIP_TAKEN;
             }
             new_tile = TILE_FLOOR;
         } else if (tile == TILE_EXIT) {
